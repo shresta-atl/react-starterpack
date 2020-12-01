48341b9ebc6554df463aecb19b6848fdf8940575
admin <admin@admins-MBP.gigaset.lan>
2020-12-01 15:18:47 +0100

    Add react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+// import './style.scss';
+
+// $('#main').html('Here we go!');
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-$('#main').html('Here we go!');
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
